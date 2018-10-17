7c6aaf64e3fe0f46a0323384433e7fa92466a4df
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50.>
2018-10-17 11:44:36 +0200

    Au commencement (il n'y avait rien) - un peu de JAïvA - ajout du jdbc sqlite

---
+++ src/java/gestionAme/DefAme.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionAme;
+
+/**
+ *
+ * @author LeRoux
+ */
+public class DefAme {
+
+    public String prenom;
+    public String nom;
+    public int karma;
+    
+    public DefAme(String prenom, String nom, int karma) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.karma = karma;
+    }
+    
+}
