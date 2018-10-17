6a36f73e3bdc65b6a48bbc08158a6bfa8de8ec02
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50.>
2018-10-17 16:01:22 +0200

    sdfoigzef

--- src/java/web/vue.java
+++ src/java/web/vue.java
@@ -24,6 +24,7 @@ public class vue implements Serializable {
     @EJB
     TrucFacadeLocal tableDAO;
     
+    Truc table;
     /**
      * Creates a new instance of vue
      */
@@ -32,7 +33,18 @@ public class vue implements Serializable {
     
     public String getValeur() {
             Truc machin = tableDAO.find(1);
-            return machin.getNom();
-        }
+            return machin.getPrenom();
+    }
     
+    public void createEtu() {
+        table = new Truc();
+    }
+
+    public Truc getTable() {
+        return table;
+    }
+
+    public void setTable(Truc table) {
+        this.table = table;
+    }
 }
